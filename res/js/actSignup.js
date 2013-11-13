00a91db897de65a8d8e17777d44e7f36f9f0ba67
imGiantChan <530000693@qq.com>
2013-11-13 22:37:14 +0800

    only a page left

---
+++ res/js/actSignup.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+function changeOutline0()
+{
+	document.getElementById("name").style.outlineColor="#157eff";
+	document.getElementById("name").style.outlineWidth="3px";
+}
+function changeOutline1()
+{
+	document.getElementById("custom").style.outlineColor="#157eff";
+}
+function changeOutline2()
+{
+	document.getElementById("carModel").style.outlineColor="#157eff";
+}
+function changeOutline3()
+{
+	document.getElementById("carSeries").style.outlineColor="#157eff";
+}
---
+++ res/js/carOnSell.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function changeOutline0()
+{
+	document.getElementById("name").style.outlineColor="#157eff";
+	document.getElementById("name").style.outlineWidth="3px";
+}
+function changeOutline1()
+{
+	document.getElementById("custom").style.outlineColor="#157eff";
+}
+function changeOutline2()
+{
+	document.getElementById("carModel").style.outlineColor="#157eff";
+}
